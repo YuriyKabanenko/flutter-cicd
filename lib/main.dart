bcd166c6fedb3dd8e3764c28830d2b00fc461135
Yuriy <kabanpuxluy@gmail.com>
2023-11-12 14:19:01 +0200

    add text changing instead of incrementing value

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,6 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -31,40 +30,45 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  final List<String> _wordList = ["HELLO", "WORLD", "HOW", "ARE", "YOU"];
+  String _text = "";
+  int index = -1;
 
-  void _incrementCounter() {
+  void _changeText() {
     setState(() {
-      _counter++;
+      if (index == _wordList.length - 1) {
+        index = -1;
+        _text = "";
+        return;
+      }
+      index++;
+      _text += '${_wordList[index]} ';
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: const Text("CI-CD"),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-    );
+        body: GestureDetector(
+          onTap: () {
+            _changeText();
+          },
+          child: Center(
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  (_text.isEmpty) ? 'Tap screen to show text' : _text,
+                  style: Theme.of(context).textTheme.headlineMedium,
+                ),
+              ],
+            ),
+          ),
+        ));
   }
 }
